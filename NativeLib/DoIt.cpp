f47bd5315de6883f05ea6d25c8f29e70f1dcb2c9
Ben <ben@ben-rush.net>
2016-09-16 20:27:47 -0500

    More updates.

--- NativeLib/DoIt.cpp
+++ NativeLib/DoIt.cpp
@@ -8,32 +8,6 @@ LONG WINAPI VectoredHandler(
     PEXCEPTION_POINTERS ExceptionInfo
 )
 {
-
-    //HANDLE hFile = CreateFileA("MiniDump.dmp", GENERIC_READ | GENERIC_WRITE,
-    //    0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-    //if ((hFile != NULL) && (hFile != INVALID_HANDLE_VALUE))
-    //{
-    //    // Create the minidump 
-    //    std::cout << "Opened file." << std::endl; 
-    //    MINIDUMP_EXCEPTION_INFORMATION mdei;
-
-    //    mdei.ThreadId = GetCurrentThreadId();
-    //    mdei.ExceptionPointers = ExceptionInfo;
-    //    mdei.ClientPointers = FALSE;
-
-    //    MINIDUMP_TYPE mdt = MiniDumpNormal;
-
-    //    std::cout << "write mini dump" << std::endl; 
-
-    //    BOOL rv = MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
-    //        hFile, mdt, (ExceptionInfo != 0) ? &mdei : 0, 0, 0);
-
-    //    std::cout << "Successfully wrote mini dump: " << rv << std::endl; 
-    //    CloseHandle(hFile);
-    //    exit(0);
-    //}
-
     HMODULE hDbgHelp = NULL;
     HANDLE hFile = NULL;
     MINIDUMP_EXCEPTION_INFORMATION mei;
