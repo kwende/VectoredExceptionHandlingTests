83bbbbaf611127f6d22c8d99833e2d0b3a788715
Ben <ben@ben-rush.net>
2016-09-16 19:15:41 -0500

    Evidence that in managed mode it doesn't work right.

---
+++ BlowerUpperNative/BlowerUpperNative.cpp
@@ -0,0 +1,12 @@
+// BlowerUpperNative.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "DoIt.h"
+
+int main()
+{
+    ::DoIt::RegisterAndThenBlowUp();
+    return 0;
+}
+
--- NativeLib/DoIt.cpp
+++ NativeLib/DoIt.cpp
@@ -4,35 +4,120 @@
 #include <DbgHelp.h>
 #include <iostream>
 
-LONG CALLBACK VectoredHandler(
-    _In_ PEXCEPTION_POINTERS ExceptionInfo
+LONG WINAPI VectoredHandler(
+    PEXCEPTION_POINTERS ExceptionInfo
 )
 {
-    HANDLE hFile = CreateFileA("MiniDump.dmp", GENERIC_READ | GENERIC_WRITE,
-        0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
-    if ((hFile != NULL) && (hFile != INVALID_HANDLE_VALUE))
+    //HANDLE hFile = CreateFileA("MiniDump.dmp", GENERIC_READ | GENERIC_WRITE,
+    //    0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    //if ((hFile != NULL) && (hFile != INVALID_HANDLE_VALUE))
+    //{
+    //    // Create the minidump 
+    //    std::cout << "Opened file." << std::endl; 
+    //    MINIDUMP_EXCEPTION_INFORMATION mdei;
+
+    //    mdei.ThreadId = GetCurrentThreadId();
+    //    mdei.ExceptionPointers = ExceptionInfo;
+    //    mdei.ClientPointers = FALSE;
+
+    //    MINIDUMP_TYPE mdt = MiniDumpNormal;
+
+    //    std::cout << "write mini dump" << std::endl; 
+
+    //    BOOL rv = MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
+    //        hFile, mdt, (ExceptionInfo != 0) ? &mdei : 0, 0, 0);
+
+    //    std::cout << "Successfully wrote mini dump: " << rv << std::endl; 
+    //    CloseHandle(hFile);
+    //    exit(0);
+    //}
+
+    HMODULE hDbgHelp = NULL;
+    HANDLE hFile = NULL;
+    MINIDUMP_EXCEPTION_INFORMATION mei;
+    MINIDUMP_CALLBACK_INFORMATION mci;
+
+    // Load dbghelp.dll
+    hDbgHelp = LoadLibraryA("dbghelp.dll");
+    if (hDbgHelp == NULL)
     {
-        // Create the minidump 
-        std::cout << "Opened file." << std::endl; 
-        MINIDUMP_EXCEPTION_INFORMATION mdei;
-
-        mdei.ThreadId = GetCurrentThreadId();
-        mdei.ExceptionPointers = ExceptionInfo;
-        mdei.ClientPointers = FALSE;
-
-        MINIDUMP_TYPE mdt = MiniDumpNormal;
-
-        std::cout << "write mini dump" << std::endl; 
-
-        BOOL rv = MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(),
-            hFile, mdt, (ExceptionInfo != 0) ? &mdei : 0, 0, 0);
-
-        std::cout << "Successfully wrote mini dump: " << rv << std::endl; 
-        CloseHandle(hFile);
-        exit(0);
+        // Error - couldn't load dbghelp.dll
+        std::cout << "Couldn't load dbhelp.dll" << std::endl; 
+        return EXCEPTION_CONTINUE_EXECUTION;
     }
-    std::cout << "."; 
+
+    // Create the minidump file
+    hFile = CreateFileA(
+        "crashdump.dmp",
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+    {
+        //std::cout << "Couldn't create file. Error " << ::GetLastError() << std::endl; 
+        // Couldn't create file
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+
+    // Write minidump to the file
+    mei.ThreadId = GetCurrentThreadId();
+    mei.ExceptionPointers = ExceptionInfo;
+    mei.ClientPointers = FALSE;
+    mci.CallbackRoutine = NULL;
+    mci.CallbackParam = NULL;
+
+    typedef BOOL(WINAPI *LPMINIDUMPWRITEDUMP)(
+        HANDLE hProcess,
+        DWORD ProcessId,
+        HANDLE hFile,
+        MINIDUMP_TYPE DumpType,
+        CONST PMINIDUMP_EXCEPTION_INFORMATION ExceptionParam,
+        CONST PMINIDUMP_USER_STREAM_INFORMATION UserEncoderParam,
+        CONST PMINIDUMP_CALLBACK_INFORMATION CallbackParam);
+
+    LPMINIDUMPWRITEDUMP pfnMiniDumpWriteDump =
+        (LPMINIDUMPWRITEDUMP)GetProcAddress(hDbgHelp, "MiniDumpWriteDump");
+    if (!pfnMiniDumpWriteDump)
+    {
+        std::cout << "Bad MiniDumpWriteDump function" << std::endl; 
+        // Bad MiniDumpWriteDump function
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+
+    HANDLE hProcess = GetCurrentProcess();
+    DWORD dwProcessId = GetCurrentProcessId();
+
+    std::cout << "Writing the dump file..." << std::endl; 
+    BOOL bWriteDump = pfnMiniDumpWriteDump(
+        hProcess,
+        dwProcessId,
+        hFile,
+        MiniDumpNormal,
+        &mei,
+        NULL,
+        &mci);
+    std::cout << "...done" << std::endl; 
+
+    if (!bWriteDump)
+    {
+        std::cout << "Error writing dump." << std::endl; 
+        // Error writing dump.
+        return EXCEPTION_CONTINUE_EXECUTION;
+    }
+
+    // Close file
+    CloseHandle(hFile);
+
+    // Unload dbghelp.dll
+    FreeLibrary(hDbgHelp);
+    std::cout << ".";
+
     return EXCEPTION_CONTINUE_EXECUTION;
 }
 
@@ -40,8 +125,11 @@ void DoIt::RegisterAndThenBlowUp()
 {
     // register the handler. 
     ::AddVectoredExceptionHandler(1, VectoredHandler); 
+    //::SetUnhandledExceptionFilter(VectoredHandler); 
 
     // blow up. null reference. 
-    int* v = 0; 
-    std::cout << *v << std::endl; 
+    //int* v = 0; 
+    //std::cout << *v << std::endl;
+
+    RaiseException(123, EXCEPTION_NONCONTINUABLE, 0, NULL);
 }
